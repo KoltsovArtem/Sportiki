39a30f7a7882a073b40041430509ea59a23f73e3
KoltsovArtem <artemklcv@gmail.com>
2022-12-23 16:41:04 +0300

    New window and some corrections

---
+++ WpfApplication1/Windows/MenuWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WpfApplication1.Windows
+{
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
