15c8ad7ee87435dcedb78b9d9f4c585c43b83736
KoltsovArtem <artemklcv@gmail.com>
2022-12-24 23:55:40 +0300

    This damn button is working now

--- WpfApplication1/Windows/ResultsWindow.xaml.cs
+++ WpfApplication1/Windows/ResultsWindow.xaml.cs
@@ -4,9 +4,33 @@ namespace WpfApplication1.Windows
 {
     public partial class ResultsWindow : Window
     {
+        int i = 0; 
+        private void Open()
+        {
+
+        }
+
         public ResultsWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (i == 0)
+            {
+                GridMain.Margin = new Thickness(200, 60, 10, 10);
+                textOrganiz.Margin = new Thickness(200, 0, 0, 0);
+                Panel.Margin = new Thickness(0, 0, 835, 0);
+                i = 1;
+            }
+            else
+            {
+                GridMain.Margin = new Thickness(60, 60, 10, 10);
+                textOrganiz.Margin = new Thickness(60, 0, 0, 0);
+                Panel.Margin = new Thickness(0, 0, 975, 0);
+                i = 0;
+            }
         }
     }
 }
