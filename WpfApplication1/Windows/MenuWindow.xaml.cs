c1832e596beb3f243afabb25bba96248ef6c56a4
KoltsovArtem <artemklcv@gmail.com>
2022-12-24 18:24:48 +0300

    MenuWindow is done, ResultWindow is created

--- WpfApplication1/Windows/MenuWindow.xaml.cs
+++ WpfApplication1/Windows/MenuWindow.xaml.cs
@@ -4,6 +4,7 @@ namespace WpfApplication1.Windows
 {
     public partial class MenuWindow : Window
     {
+        int i = 0;
         public MenuWindow()
         {
             InitializeComponent();
@@ -11,23 +12,39 @@ namespace WpfApplication1.Windows
 
         private void StartProtocols_Click(object sender, RoutedEventArgs e)
         {
-
+            StartProtocolsLabel.Content = "Стартовые протколы сформированы";
+            StartProtocols.Visibility = Visibility.Collapsed;
+            if (i == 2) SeeResults.Visibility = Visibility.Visible;
+            else i += 1;
         }
 
         private void ResultProtocols_Click(object sender, RoutedEventArgs e)
         {
-
+            ResultProtocolsLabel.Content = "Протоколы результатов сформированы";
+            ResultProtocols.Visibility = Visibility.Collapsed;
+            if (i == 2) SeeResults.Visibility = Visibility.Visible;
+            else i += 1;
         }
 
         private void GroupsResultProtocols_Click(object sender, RoutedEventArgs e)
         {
-
+            GroupsResultProtocolsLabel.Content = "Протоколы результатов команд сформированы";
+            GroupsResultProtocols.Visibility = Visibility.Collapsed;
+            if (i == 2) SeeResults.Visibility = Visibility.Visible;
+            else i += 1;
         }
 
-        private void GoToStart_Click(object sender, RoutedEventArgs e)
+        private void BackUp_Click(object sender, RoutedEventArgs e)
         {
             MainWindow mainW = new MainWindow();
             mainW.Show();
+            this.Close();
+        }
+
+        private void SeeResults_Click(object sender, RoutedEventArgs e)
+        {
+            ResultsWindow res = new ResultsWindow();
+            res.Show();
             this.Close();
         }
     }
---
+++ WpfApplication1/Windows/ResultsWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WpfApplication1.Windows
+{
+    public partial class ResultsWindow : Window
+    {
+        public ResultsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
